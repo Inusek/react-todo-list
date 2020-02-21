d0933d01162fa69dc4c570bb7cd58ed4adb53339
unknown <arkadiusz.micek@gmail.com>
2020-02-21 01:49:18 +0100

    App component structure

--- src/App.js
+++ src/App.js
@@ -6,11 +6,60 @@ import TodoList from "./components/TodoList";
 
 // Showing vs-codegithub setup
 export default class App extends Component {
+    state = {
+        items: [
+            {
+                id: 1,
+                title: "Wake up"
+            },
+            {
+                id: 2,
+                title: "breakfast"
+            }
+        ],
+        id: uuid(),
+        item: "",
+        editItem: false
+    };
+
+    handleChange = e => {
+        console.log("handle change");
+    };
+    handleSubmit = e => {
+        console.log("handle submit");
+    };
+    clearList = () => {
+        console.log("Clear list method");
+    };
+    handleDelete = id => {
+        console.log(`handle edit ${id}`);
+    };
+    handleEdit = id => {
+        console.log(`edit ${id}`);
+    };
+
     render() {
         return (
-            <div>
-                <TodoInput />
-                <TodoList />
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto col-md-8 mt-5">
+                        <h3 className="text-capitalize text-center">
+                            todo input
+                        </h3>
+                        <TodoInput
+                            item={this.state.item}
+                            handleChange={this.handleChange}
+                            handleSubmit={this.handleSubmit}
+                            editItem={this.state.editItem}
+                        />
+                        <TodoList
+                            item={this.state.items}
+                            clearList={this.clearList}
+                            handleDelete={this.handleDelete}
+                            handleEdit={this.handleEdit}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
